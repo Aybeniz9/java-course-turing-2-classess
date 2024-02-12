ec72bbda327a18cd74f909ba378e89f0d84481a4
aybeniz <aybenizkazimova15@gmail.com>
2024-02-12 20:27:17 +0400

    Month 2

---
+++ src/main/java/turing/java/edu/az/InfotmationApp.java
@@ -0,0 +1,36 @@
+package turing.java.edu.az;
+
+import java.util.Scanner;
+import javax.swing.*;
+
+public class InfotmationApp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String[] name = new String[n];
+        int[] age = new int[n];
+        int[] grade = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.println(i + "-ci adi Daxil edin ");
+            name[i] = sc.nextLine();
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.println(i + "-ci yasi daxil edin ");
+            age[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.println(i + "-ci grade daxil edin ");
+
+            grade[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            int max = grade[0];
+            if (grade[i] > max) {
+                max = grade[i];
+            }
+            System.out.println(max);
+
+        }
+
+    }
+}
--- src/main/java/turing/java/edu/az/Main.java
+++ src/main/java/turing/java/edu/az/Main.java
@@ -2,6 +2,9 @@ package turing.java.edu.az;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+
+
     }
 }
