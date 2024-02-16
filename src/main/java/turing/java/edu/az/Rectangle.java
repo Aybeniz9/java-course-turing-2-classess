b94f03fd41f4b274cd1fd644112ec8e40046a6d5
aybeniz <aybenizkazimova15@gmail.com>
2024-02-17 01:18:54 +0400

    Month 2

--- src/main/java/turing/java/edu/az/Rectangle.java
+++ src/main/java/turing/java/edu/az/Rectangle.java
@@ -4,5 +4,6 @@ public class Rectangle {
     public static void main(String[] args) {
        RectangleApp rectangleApp=new RectangleApp (25, 45,"White",3);
         System.out.println(rectangleApp.getCountOfRectangle());
+
     }
 }
