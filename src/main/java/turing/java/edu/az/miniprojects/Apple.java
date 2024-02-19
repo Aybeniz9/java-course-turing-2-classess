bca634ff5d91e370e748dd39a00f3ea58c9b88f6
aybeniz <aybenizkazimova15@gmail.com>
2024-02-19 19:09:25 +0400

    Lab

---
+++ src/main/java/turing/java/edu/az/miniprojects/Apple.java
@@ -0,0 +1,27 @@
+package turing.java.edu.az.miniprojects;
+
+public class Apple {
+
+     public  String nationality;
+
+     public void eatApple( ){
+         System.out.println("Alma yeyilmisdir");
+     }
+     public void giveApple(){
+        System.out.println("Marketden alma alinmisdir");
+    }
+    public void soyApple(){
+        System.out.println("Alma soyulmusdur");
+    }
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public Apple(String nationality) {
+        this.nationality = nationality;
+    }
+}
---
+++ src/main/java/turing/java/edu/az/miniprojects/Fruit.java
@@ -0,0 +1,27 @@
+package turing.java.edu.az.miniprojects;
+
+public class Fruit {
+    public String color;
+    public int kq;
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getKq() {
+        return kq;
+    }
+
+    public Fruit(String color, int kq) {
+        this.color = color;
+        this.kq = kq;
+    }
+
+
+    public static void main(String[] args) {
+        Apple apple=new Apple("Quba");
+        System.out.println(apple.nationality);
+        Fruit fruit=new Fruit("Qirmizi",5);
+    }
+
+}
