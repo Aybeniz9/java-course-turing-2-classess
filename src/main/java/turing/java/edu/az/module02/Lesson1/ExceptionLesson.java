d8c59f69af5984d747ee8c792ec974bdf7f5ef2e
aybeniz <aybenizkazimova15@gmail.com>
2024-03-11 20:25:42 +0400

    MiniProject

---
+++ src/main/java/turing/java/edu/az/module02/Lesson1/ExceptionLesson.java
@@ -0,0 +1,22 @@
+package turing.java.edu.az.module02.Lesson1;
+import java.util.Scanner;
+public class ExceptionLesson {
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        String surname = sc.nextLine();
+        int n1 = Integer.parseInt(name);
+
+        System.out.println(name);
+        int n2=Integer.parseInt(surname);
+try{}
+
+catch (Exception e){
+    System.out.println(n1/n2);
+
+}
+
+    }
+}
